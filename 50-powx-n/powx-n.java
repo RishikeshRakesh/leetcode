21d79812b8b6c10624202b5b4ba97218c0f289be
Rishikesh <142293326+RishikeshRakesh@users.noreply.github.com>
2024-07-09 22:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (95.16%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double myPow(double x, int n) {
+        int sum=0;
+         //if(n>0)
+         return Math.pow(x,n);
+        //  else if(n%2==0)
+        //  return Math.pow()
+    
+}}
