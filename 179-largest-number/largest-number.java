ab79ee0f447ec34b257d9081f85b8f3b273bdd9e
Rishikesh <142293326+RishikeshRakesh@users.noreply.github.com>
2024-08-17 11:21:09 +0530

    Time: 5 ms (98.71%) | Memory: 43.2 MB (54.20%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,35 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
     public String largestNumber(int[] nums) {
-        // Convert integers to strings
-        String[] numStrs = new String[nums.length];
+        String[] num = new String[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            numStrs[i] = String.valueOf(nums[i]);
+            num[i] = String.valueOf(nums[i]);
         }
 
-        // Perform a bubble sort-like sorting with a custom comparator
-        for (int i = 0; i < numStrs.length - 1; i++) {
-            for (int j = i + 1; j < numStrs.length; j++) {
-                // Compare concatenated results to determine order
-                String order1 = numStrs[i] + numStrs[j];
-                String order2 = numStrs[j] + numStrs[i];
-                if (order2.compareTo(order1) > 0 ) {
-                    // Swap numStrs[i] and numStrs[j]
-                    String temp = numStrs[i];
-                    numStrs[i] = numStrs[j];
-                    numStrs[j] = temp;
-                }
-            }
-        }
+       
+       Arrays.sort(num,(a,b)->(b+a).compareTo(a+b));
 
-        // Handle edge case: if the largest number is "0", return "0"
-        if (numStrs[0].equals("0")) {
+        if (num[0].equals("0")) {
             return "0";
         }
 
-        // Concatenate sorted strings to form the largest number
         StringBuilder sb = new StringBuilder();
-        for (String numStr : numStrs) {
-            sb.append(numStr);
+        for (String numss : num) {
+            sb.append(numss);
         }
 
         return sb.toString();
