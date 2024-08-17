c1b0b7a63ce8491d038123969fbba1157dad8f40
Rishikesh <142293326+RishikeshRakesh@users.noreply.github.com>
2024-08-17 14:01:32 +0530

    Time: 1 ms (98.80%) | Memory: 41.7 MB (23.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,36 +1,25 @@
 class Solution {
-    public static boolean isValid(String s) {
-     Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } 
-            
-            else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } 
-            
-            else {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        for(char c:s.toCharArray())
+        {
+            if(c=='(')
+            {
+                st.push(')');
+            }
+            else if(c=='{')
+            {
+                st.push('}');
+            }
+            else if(c=='[')
+            {
+                st.push(']');
+            }
+            else if(st.isEmpty()|| st.pop()!=c)
+            {
                 return false;
             }
         }
-       
-        return stack.isEmpty();
-    }   
-    
-    public static void main(String[] args) {
-        String para = "()";
-        String para1 = "()[]{}";
-        String para2 = "(]";
-        
-        System.out.println(isValid(para));
-        System.out.println(isValid(para1));
-        System.out.println(isValid(para2)); 
+        return st.isEmpty();
     }
 }
