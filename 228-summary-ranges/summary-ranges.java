5545c7340e973c4d52ba63885c6a86f4a49d64ec
Rishikesh <142293326+RishikeshRakesh@users.noreply.github.com>
2024-08-20 19:57:03 +0530

    Time: 6 ms (34.76%) | Memory: 41.6 MB (49.22%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list=new ArrayList<>();
+        int s=0;
+        for(int i=0;i<nums.length;i++){
+            if(i==nums.length-1){
+                if(i==s){
+                        String s1=String.valueOf(nums[i]);
+                        list.add(s1);
+                    }
+                    else{
+                        String p=""+nums[s];
+                        String p1=""+nums[i];
+                        String p2=p+"->"+p1;
+                        list.add(p2);
+                        break;
+                    }
+
+            }
+            else{
+                if(nums[i]+1==nums[i+1]){
+                    continue;
+                }
+                else{
+                    if(i==s){
+                        String s1=String.valueOf(nums[i]);
+                        list.add(s1);
+                        s=i+1;
+                    }
+                    else{
+                        String p=""+nums[s];
+                        String p1=""+nums[i];
+                        String p2=p+"->"+p1;
+                        list.add(p2);
+                        s=i+1;
+                    }
+                }
+            }
+        }
+        return list;
+    } 
+}
