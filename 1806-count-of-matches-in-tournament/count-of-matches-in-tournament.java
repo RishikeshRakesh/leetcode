9c7c969a8e681df01a6dcae6e010983ed95f1997
Rishikesh <142293326+RishikeshRakesh@users.noreply.github.com>
2024-09-15 15:10:52 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (5.49%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,12 +1,6 @@
 class Solution {
     public int numberOfMatches(int n) {
-        // int num=0;
-        // int val=0;
-        // while(n!=0){
-        //       num=n/2;
-        //       n=n-num;
-        //       val+=n;
-        // }
+        
         return n-1;
     }
 }
